3933d06fe09efe82a993dafe4ec1e02f9cac2ade
Pratik <impratikbhowmik@gmail.com>
2022-06-04 21:05:28 +0530

    Files added

--- app/src/test/java/instagram/Facebook.java
+++ app/src/test/java/instagram/Facebook.java
@@ -8,41 +8,39 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Facebook {
     private WebDriver driver;
     private Alert al;
 
-    public void launchFacebook(){
+    public void launchFacebook() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.facebook.com/");
         driver.manage().window().maximize();
-        try{
+        try {
             driver.findElement(By.name("email")).sendKeys("impratikbhowmik@gmail.com");
             driver.findElement(By.name("pass")).sendKeys("Patrick@12oct2020");
             driver.findElement(By.name("login")).click();
-        }catch(NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             System.out.println("Element not found");
         }
-        try{
+        try {
             al = driver.switchTo().alert();
             al.accept();
-        }catch(NoAlertPresentException e){
+        } catch (NoAlertPresentException e) {
             System.out.println("Alert exception handled");
         }
 
     }
 
-    public void likeApost(){
-        try{
+    public void likeApost() {
+        try {
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("window.scrollBy(0,350)", "");
-        }catch(JavascriptException e){
+        } catch (JavascriptException e) {
             System.out.println("Exception handled");
         }
     }
-    
 }
--- app/src/test/java/instagram/Insta.java
+++ app/src/test/java/instagram/Insta.java
@@ -1,4 +1,5 @@
 package instagram;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -7,25 +8,46 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Insta {
     private WebDriver driver;
-    public void launch(){
+
+    public void launch() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.instagram.com/");
         driver.manage().window().maximize();
-    }
-    public void loginToInsta(){
-        try{
-            driver.findElement(By.xpath("//input[@name='username']")).sendKeys("impratikbhowmik@gmail.com");
-        }catch(NoSuchElementException e){
-            System.out.println("Hey check your element locator again");
-        }finally{
-            System.out.println("LOL! why did you use finally block!");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println("handled");
         }
     }
-    public void likeApost(){
-        
+
+    public void loginToInsta() {
+        try {
+            driver.findElement(By.xpath("//input[@name='username']")).sendKeys("impratikbhowmik@gmail.com");
+            driver.findElement(By.name("password")).sendKeys("Patrick@12oct2020");
+            driver.findElement(By.xpath("//button[@class='sqdOP  L3NKy   y3zKF     ']")).click();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println("Handled");
+            }
+            driver.findElement(By.xpath("//button[@class='sqdOP yWX7d    y3zKF     ']")).click();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println("Handled");
+            }
+            driver.findElement(By.xpath("//button[@class='_a9-- _a9_1']")).click();
+        } catch (NoSuchElementException e) {
+            System.out.println("Hey check your element locator again");
+        }
     }
-    public void closeBrowser(){
+
+    public void likeApost() {
+
+    }
+
+    public void closeBrowser() {
         driver.quit();
     }
 }
